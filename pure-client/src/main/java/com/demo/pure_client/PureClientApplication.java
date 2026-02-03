e8d99e7df7ff0e388ebf5a909ea60a78b9cdcc53
Pranjal Singh <155311442+06Pranjal@users.noreply.github.com>
2026-02-03 23:44:05 +0530

    Pure Client Authorization request

--- pure-client/src/main/java/com/demo/pure_client/PureClientApplication.java
+++ pure-client/src/main/java/com/demo/pure_client/PureClientApplication.java
@@ -1,8 +1,13 @@
 package com.demo.pure_client;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.oauth2.client.*;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.client.RestTemplate;
@@ -36,6 +41,36 @@ public class PureClientApplication {
 		manager.setAuthorizedClientProvider(provider);
 		return manager;
 
+	}
+
+	@Bean
+	public CommandLineRunner run(
+			OAuth2AuthorizedClientManager manager,
+			RestTemplate rest,
+			@Value("${service2.url}") String service2Url
+			) {
+		return args -> {
+			var authRequest = OAuth2AuthorizeRequest
+					.withClientRegistrationId("keycloak-client")
+					.principal("machine")
+					.build();
+
+			var client = manager.authorize(authRequest);
+			String token = client.getAccessToken().getTokenValue();
+
+			HttpHeaders headers = new HttpHeaders();
+			headers.setBasicAuth(token);
+
+			var resp=rest.exchange(
+					service2Url + "/data",
+					HttpMethod.GET,
+					new HttpEntity<>(headers),
+					String.class
+			);
+
+			System.out.println("Response From Service 2: " + resp);
+
+		};
 	}
 
 }
